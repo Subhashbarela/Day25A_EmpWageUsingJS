9c6b27fcf844d69b03c0bd5d338c28103c3bddab
Subhash Barela <barelasubhash16@gmail.com>
2023-06-03 12:33:03 +0530

    [Subhash] Find EmpWage Based On NumberOfWorking Days

--- BasicProgramUsingJs/Employee.js
+++ BasicProgramUsingJs/Employee.js
@@ -4,26 +4,36 @@ let FULL_TIME_HR = 8;
 let PART_TIME_HR = 4;
 let WAGE_PER_HR = 20;
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 3);
+let NUM_OF_WORKING_DAYS = 10;
+let fullTime = 0;
+let partTime = 0;
+let Absent = 0;
 
-switch (empCheck)
- {
-    case IS_FULL_TIME:
-        {
-            console.log("Full Time Employee")
-            empHrs = FULL_TIME_HR;
-            break;
-        }
-    case IS_PART_TIME:
-        {
-            console.log("Part Time Employee");
-            empHrs = PART_TIME_HR;
-            break;
-        }
+for (var i = 0; i < NUM_OF_WORKING_DAYS; i++) {
+    let empCheck = Math.floor(Math.random() * 3);
+
+    switch (empCheck) {
+        case IS_FULL_TIME:
+            {
+                fullTime++;
+                empHrs += FULL_TIME_HR;
+                break;
+            }
+        case IS_PART_TIME:
+            {
+                partTime++;
+                empHrs += PART_TIME_HR;
+                break;
+            }
         default: console.log("Employee is absent");
-        empHrs=0;
-        break;
+            Absent++;
+            empHrs += 0;
+            break;
+    }
 }
-let empWage=empHrs * WAGE_PER_HR;
-console.log("EmpWage Is : "+empWage);
+let empWage = empHrs * WAGE_PER_HR;
+console.log("total full time : "+fullTime);
+console.log("total part time : "+partTime);
+console.log("total absent  : "+Absent);
+console.log("EmpWage Is : " + empWage);
 
