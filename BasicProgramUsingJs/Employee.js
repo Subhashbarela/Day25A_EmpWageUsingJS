8d2bf5671e33e9db045057d7285356a7d3490b66
Subhash Barela <barelasubhash16@gmail.com>
2023-06-03 12:16:09 +0530

    [Subhash] Check Part And Full Time Emp

--- BasicProgramUsingJs/Employee.js
+++ BasicProgramUsingJs/Employee.js
@@ -1,10 +1,29 @@
-let IS_PRESENT=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(IS_PRESENT=empCheck)
-{
- console.log("Employee is Present");
+let IS_FULL_TIME = 1;
+let IS_PART_TIME = 2;
+let FULL_TIME_HR = 8;
+let PART_TIME_HR = 4;
+let WAGE_PER_HR = 20;
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 3);
+
+switch (empCheck)
+ {
+    case IS_FULL_TIME:
+        {
+            console.log("Full Time Employee")
+            empHrs = FULL_TIME_HR;
+            break;
+        }
+    case IS_PART_TIME:
+        {
+            console.log("Part Time Employee");
+            empHrs = PART_TIME_HR;
+            break;
+        }
+        default: console.log("Employee is absent");
+        empHrs=0;
+        break;
 }
-else
-{
-    console.log("Employee is absent:");
-}
+let empWage=empHrs * WAGE_PER_HR;
+console.log("EmpWage Is : "+empWage);
+
